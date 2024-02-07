3e4d27a496cde2096dd032c07d31807d4c18b606
DanTiw <danishtiwari2204@gmail.com>
2024-02-07 22:01:35 +0530

    adding Buttons css

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,27 +2,14 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
-import notes from "../notes";
-
-function createEntry(note) {
-  return <Note 
-  key={note.id} 
-  title={note.name} 
-  content={note.info} 
-  />;
-}
+import CreateArea from "./CreateArea";
 
 function App() {
   return (
     <div>
       <Header />
-      {notes.map((noteItem) => (
-        <Note
-          key={noteItem.key}
-          title={noteItem.title}
-          content={noteItem.content}
-        />
-      ))}
+      <CreateArea />
+      <Note key={1} title="Note title" content="Note content" />
       <Footer />
     </div>
   );
--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -4,7 +4,8 @@ function Note(props) {
   return (
     <div className="note">
       <h1>{props.title}</h1>
-      <p> {props.content}</p>
+      <p>{props.content}</p>
+      <button>DELETE</button>
     </div>
   );
 }
